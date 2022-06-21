8db78402b9b9d44533bcd7571381af7fdb73cafb
sirafr <andres22f@hotmail.com>
2022-06-20 23:46:56 -0500

    #56 Reverse: giving names to the urls

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponse, HttpResponseNotFound,Http404, HttpResponseRedirect
+from django.urls import reverse
 
 articles = {
     'sports':'Sports Page',
@@ -22,7 +23,9 @@ def num_page_view(request, num_page):
     topics_list = list(articles.keys()) # [sports,finance,politics]
     topic = topics_list[num_page]
 
-    return HttpResponseRedirect(f"{url_base}{topic}")
+    webpage = reverse('topic-page',args=[topic]) # reverse will set a name later for the urls.
+
+    return HttpResponseRedirect(webpage) # it can be inserted directly.
 
 
 
